fbf25af3148ca249a12f964254f8f224718fafa0
Isaac Santiago <isaac.arandia.mt@correo.usa.edu.co>
2021-10-17 18:16:30 -0500

    Santiago: +}Nuevo intento

--- backend/reto3/src/main/java/co/edu/usa/reto3/modelo/Message.java
+++ backend/reto3/src/main/java/co/edu/usa/reto3/modelo/Message.java
@@ -4,10 +4,47 @@
  */
 package co.edu.usa.reto3.modelo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 /**
  *
  * @author Usuario
  */
+@Entity
+@Table(name="message")
+
 public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idMessage;
+    private String messageText;
+
+  //  @ManyToOne
+    //@JoinColumn(name="categoria_id", nullable = false)
+    //@JsonIgnoreProperties("productos")
+    //private Categoria categoria;
+
+    public Integer getIdMessage() {
+        return idMessage;
+    }
+
+    public void setIdMessage(Integer idMessage) {
+        this.idMessage = idMessage;
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
     
 }
